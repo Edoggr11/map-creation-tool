06c235f4ad52aada7db5f806ae3c96701ae9a795
Evan Y <edoggr11@gmail.com>
2021-10-04 00:54:35 +0000

    partially implemented sprite placement

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
 var collumns;
 var rows;
 var cellSize = 20;
-
+var spriteRadius = 5;
 var inpColor = 1;
+var selectedCol = 'blue';
 
-var selectedCol = "blue";
+var spriteMode = false;
+var selectedSprite = 'barrel';
 
-//this is for the random colors to save to in the future
-//sus
 var colorDict = {
 	1: '#0000FF',
 };
 
-var array = new Array();
+var array = [];
+var spriteDict = {};
+var spriteNums = {};
 
+// run when the settings apply button is pressed
 function readSet() {
-	collumns = +document.getElementById("widthInp").value;
-	rows = +document.getElementById("heightInp").value;
+	collumns = +document.getElementById('widthInp').value;
+	rows = +document.getElementById('heightInp').value;
 
 	array = Array(rows).fill().map(() => Array(collumns).fill(0));
 
 	if (collumns > 0 && collumns <= 100 && Number.isInteger(+collumns) && rows > 0 && rows <= 100 && Number.isInteger(+rows)) {
+
+		spriteMode = false;
+		spriteDict = {};
 
 		createCanvas(collumns * cellSize, rows * cellSize);
 		loop();
 
 	} else {
 
-		window.alert("Invalid size");
-		console.log("Invalid size");
+		window.alert('Invalid size');
+		console.log('Invalid size');
 	}
-
 }
 
 function changeIndex(num) {
 
 	if (typeof num !== 'undefined'){
-		document.getElementById("index").value = num;
+		document.getElementById('index').value = num;
 	}
 
-	inpColor = +document.getElementById("index").value;
+	inpColor = +document.getElementById('index').value;
 
 	if (!colorDict[inpColor]){
 		colorDict[inpColor] = setRandomColor(inpColor);
@@ -51,35 +56,55 @@ function changeIndex(num) {
 		selectedCol = colorDict[inpColor];
 	}
 	selectedCol = colorDict[inpColor];
-	console.log(selectedCol);	
+	console.log(selectedCol);
+}
+
+function setActiveSprite() {
+	selectedSprite = document.getElementById('sprite').value;
 }
 
 function spawnKeys(input) {
-	var th = document.createElement("th");
-	document.getElementById("colorNumber").appendChild(th);
+	var th = document.createElement('th');
+	document.getElementById('colorNumber').appendChild(th);
 	th.textContent = input;
 
-	var colorBox = document.createElement("th");
-	var colorBoxSpan = document.createElement("span");
-	document.getElementById("colorDisplay").appendChild(colorBox);
+	var colorBox = document.createElement('th');
+	var colorBoxSpan = document.createElement('span');
+	document.getElementById('colorDisplay').appendChild(colorBox);
 	colorBox.appendChild(colorBoxSpan);
-	colorBoxSpan.setAttribute("class", "box");
-	colorBoxSpan.setAttribute("style", "background: " + colorDict[input] + ";");
-	colorBoxSpan.setAttribute("onClick", "changeIndex(" + input + ");");
+	colorBoxSpan.setAttribute('class', 'box');
+	colorBoxSpan.setAttribute('style', 'background: ' + colorDict[input] + ';');
+	colorBoxSpan.setAttribute('onClick', 'changeIndex(' + input + ');');
 }
 
+// generates a pseudo-random color based on the seed
 function setRandomColor(seed) {
-	return "#" + Math.floor((Math.abs(Math.sin(seed) * 16777215)) % 16777215).toString(16);
+	return '#' + Math.floor((Math.abs(Math.sin(seed) * 16777215)) % 16777215).toString(16);
 }
 
 function exportData() {
-	document.getElementById("exportOutput").value = JSON.stringify(array);
+	if (spriteMode === true){
+		
+		document.getElementById('exportOutput').value = "";
+		
+		for (i = 0; i < Object.keys(spriteDict).length; i++) {
+			var text;
+			text = "new Pseudo3D.Sprite(" + spriteNums[spriteDict[i][2]] + ", [" + spriteDict[i][0] + ", " + spriteDict[i][1] + "]),";
+			console.log(text);
+			
+			document.getElementById('exportOutput').value += text + '\n';
+		}
+		
+	} else {
+		document.getElementById('exportOutput').value = JSON.stringify(array);
+	}
+	
 }
 
 function importData() {
 
 	try {
-        array = JSON.parse(document.getElementById("importInput").value);
+        array = JSON.parse(document.getElementById('importInput').value);
     } catch(e) {
         alert(e);
     }
@@ -94,7 +119,7 @@ function importData() {
 
 	for (var i = 0; i < rows; i++){
 		for (var m = 0; m < collumns; m++){
-			if (array[i][m] != 0){
+			if (array[i][m] !== 0){
 				if (!colorDict[array[i][m]]) {
 					colorDict[array[i][m]] = setRandomColor(array[i][m]);
 					spawnKeys(array[i][m]);
@@ -103,14 +128,35 @@ function importData() {
 				fill(localColor);
 				rect(m * cellSize, i * cellSize, cellSize, cellSize);
 				noFill();
-			} 
+			}
+		}
+	}
+
+	document.getElementById('widthInp').value = collumns;
+	document.getElementById('heightInp').value = rows;
+
+}
+
+function drawFromArray() {
+	for (var i = 0; i < rows; i++){
+		for (var m = 0; m < collumns; m++){
+			if (array[i][m] !== 0){
+				if (!colorDict[array[i][m]]) {
+					colorDict[array[i][m]] = setRandomColor(array[i][m]);
+					spawnKeys(array[i][m]);
+				}
+				localColor = colorDict[array[i][m]];
+				fill(localColor);
+				rect(m * cellSize, i * cellSize, cellSize, cellSize);
+				noFill();
+			}
 		}
 	}
 }
 
 function setup() {
-	for (let element of document.getElementsByClassName("p5Canvas")) {
-    	element.addEventListener("contextmenu", (e) => e.preventDefault());
+	for (let element of document.getElementsByClassName('p5Canvas')) {
+    	element.addEventListener('contextmenu', (e) => e.preventDefault());
 	}
 	noLoop();
 	background(255);
@@ -119,6 +165,13 @@ function setup() {
 }
 
 function draw() {
+
+	// remove sprite dots when not in spritemode
+	if (!spriteMode) {
+		background('#FF');
+	}
+
+	// draws the tile dividers
 	for (var i = 0; i < collumns; i++) {
 		for (var m = 0; m < rows; m++) {
 			var x = i * cellSize;
@@ -126,24 +179,82 @@ function draw() {
 			rect(x, y, cellSize, cellSize);
 		}
 	}
+
+	// draw the existing boxes from the array
+	drawFromArray();
+
+	// places and removes tiles
 	if (mouseIsPressed && mouseX <= width - 1 && mouseX >= 0 && mouseY <= height - 1 && mouseY >= 0) {
 		if (mouseButton === LEFT) {
 			mouseDet(selectedCol, inpColor);
 		} else if (mouseButton === RIGHT) {
 			mouseDet(255, 0);
 		} else {
-			console.log("[mouse] tf u doin lmao");
+			console.log('[mouse] tf u doin lmao');
 		}
+	}
+
+	// run the spritemode
+	if (spriteMode) {
+		runSpriteMode();
 	}
 }
 
 function mouseDet(color, index) {
-	fill(color);
-	rect(mouseX - (mouseX % cellSize), mouseY - (mouseY % cellSize),cellSize, cellSize);
+	if (!spriteMode) {
+		var xIndex = Math.floor((mouseX - (mouseX % cellSize)) / cellSize);
+		var yIndex = Math.floor((mouseY - (mouseY % cellSize)) / cellSize);
+		
+		array[yIndex][xIndex] = index;
+	}
+}
+
+function runSpriteMode() {
+	fill('#FFFFFF80')
+	rect(0, 0, width, height);
+
+	for (i = 0; i < Object.keys(spriteDict).length; i++){
+		fill(setRandomColor(spriteDict[i][2]-600));
+		ellipse(spriteDict[i][0], spriteDict[i][1], spriteRadius*2);
+		
+	}
 	noFill();
 
-	var xIndex = Math.floor((mouseX - (mouseX % cellSize)) / cellSize);
-	var yIndex = Math.floor((mouseY - (mouseY % cellSize)) / cellSize);
-	
-	array[yIndex][xIndex] = index;
+}
+
+function mouseReleased() {
+	// place sprites on the screne here if in spritemode
+	if (spriteMode && mouseX <= width - 1 && mouseX >= 0 && mouseY <= height - 1 && mouseY >= 0) {
+		if (mouseButton === LEFT) {
+			// ellipse(mouseX+spriteRadius, mouseY+spriteRadius, spriteRadius*2);
+			var lowestSpriteDict = findLowestUnused(spriteDict);
+			var lowestSpriteNums = findLowestUnused(spriteNums);
+
+			// add sprite to spriteNums if it isn't already in the object
+			if (!getKeyByValue(spriteNums, selectedSprite)) {
+				console.log('new sprite color');
+				spriteNums[lowestSpriteNums] = selectedSprite;
+			}
+			if (keyIsPressed && keyCode === CONTROL) {
+				spriteDict[lowestSpriteDict] = [((Math.round((mouseX-10)/cellSize)*cellSize)+10), ((Math.round((mouseY-10)/cellSize)*cellSize)+10), getKeyByValue(spriteNums, selectedSprite)];
+				console.log('control');
+			} else {
+				spriteDict[lowestSpriteDict] = [mouseX, mouseY, getKeyByValue(spriteNums, selectedSprite)];
+			}
+			console.log(lowestSpriteDict, spriteDict[lowestSpriteDict]);
+		}
+	}
+}
+
+function getKeyByValue(object, value) {
+	return Object.keys(object).find(key => object[key] === value);
+}
+
+
+function findLowestUnused(object) {
+	var i = 0;
+	while (object[i]) {
+		i++;
+	}
+	return i;
 }
